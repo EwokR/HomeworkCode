b9ce5e0a4f25202bbbe4dda2db6fd246465bfa67
Anton <imewokrus@gmail.com>
2022-09-05 19:27:45 +0300

    First Commit

--- src/main/java/com/example/homeworkcode/controller/StudentController.java
+++ src/main/java/com/example/homeworkcode/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.Collections;
+
 
 @RequestMapping("student")
 @RestController
